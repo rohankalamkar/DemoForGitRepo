07f127e74de6b585f03b80d784f414952999b412
rohankalamkar <rohan.kalamkar7@gmail.com>
2022-01-01 15:51:46 +0530

    added sop statements

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("This is an Main Demo Project for Git");
-		
+		System.out.println("This is an Main Demo Project for Git...!!");
+		System.out.println("We have many Offers...");
 		
 	}
 
